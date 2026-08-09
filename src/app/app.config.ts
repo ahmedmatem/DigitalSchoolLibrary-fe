94d7d9472e91432dbbee9d9037de7a26318b7a9d
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-09 19:28:12 +0100

    App configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        // authInterceptor,
+        // loadingInterceptor,
+        // errorInterceptor,
+      ])
+    ),
+    
+    provideToastr({
+      positionClass: 'toast-top-right',
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true,
+      preventDuplicates: true,
+    }),
   ]
 };
