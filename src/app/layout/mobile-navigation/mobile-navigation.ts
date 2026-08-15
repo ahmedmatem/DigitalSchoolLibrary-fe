d720750234eedbe1d4f362094e6925cb75dd2d2a
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-15 10:46:33 +0100

    Create mobile-navigation component

--- src/app/layout/mobile-navigation/mobile-navigation.ts
+++ src/app/layout/mobile-navigation/mobile-navigation.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  input,
+  output,
+} from '@angular/core';
+
+import {
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+
+import {
+  LucideX,
+  LucideBookOpen,
+  LucideBookmark,
+  LucideGraduationCap,
+  LucideShield,
+  LucideUser,
+  LucideLogIn,
+} from '@lucide/angular';
+
+import { IconButton } from '../../shared/ui/icon-button/icon-button';
 
 @Component({
   selector: 'sl-mobile-navigation',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    IconButton,
+
+    LucideX,
+    LucideBookOpen,
+    LucideBookmark,
+    LucideGraduationCap,
+    LucideShield,
+    LucideUser,
+    LucideLogIn,
+  ],
   templateUrl: './mobile-navigation.html',
   styleUrl: './mobile-navigation.scss',
 })
-export class MobileNavigation {}
+export class MobileNavigation {
+  readonly open = input(false);
+
+  readonly authenticated = input(false);
+
+  readonly displayName = input<string | null>(null);
+
+  readonly role = input<'Student' | 'Teacher' | 'Admin' | null>(null);
+
+  readonly close = output<void>();
+}
