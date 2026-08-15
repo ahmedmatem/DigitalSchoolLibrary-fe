e6c018d708b74f63cadc7837b3a7e0e07b836660
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-15 10:38:35 +0100

    Update AppHeader with lucide icons

--- src/app/layout/app-header/app-header.ts
+++ src/app/layout/app-header/app-header.ts
@@ -1,14 +1,27 @@
-import { Component, input, output } from '@angular/core';
+import {
+  Component,
+  input,
+  output,
+} from '@angular/core';
+
 import {
   RouterLink,
   RouterLinkActive,
 } from '@angular/router';
+
+import {
+  LucideMenu,
+} from '@lucide/angular';
+
+import { IconButton } from '../../shared/ui/icon-button/icon-button';
 
 @Component({
   selector: 'sl-app-header',
   imports: [
     RouterLink,
     RouterLinkActive,
+    IconButton,
+    LucideMenu,
   ],
   templateUrl: './app-header.html',
   styleUrl: './app-header.scss',
@@ -18,7 +31,12 @@ export class AppHeader {
 
   readonly displayName = input<string | null>(null);
 
-  readonly role = input<'Student' | 'Teacher' | 'Admin' | null>(null);
+  readonly role = input<
+    'Student' |
+    'Teacher' |
+    'Admin' |
+    null
+  >(null);
 
   readonly menuClick = output<void>();
 }
