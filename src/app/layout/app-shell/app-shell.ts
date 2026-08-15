943b8b82a453642e0e2d155d1e3eb5f487de5ba0
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-15 10:51:44 +0100

    Add mobile navigation in app-shell

--- src/app/layout/app-shell/app-shell.ts
+++ src/app/layout/app-shell/app-shell.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { AppHeader } from '../app-header/app-header';
+import { MobileNavigation } from '../mobile-navigation/mobile-navigation';
 
 @Component({
   selector: 'sl-app-shell',
   imports: [
     RouterOutlet,
     AppHeader,
+    MobileNavigation,
   ],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.scss',
 })
-export class AppShell {}
+export class AppShell {
+  readonly mobileMenuOpen = signal(false);
+
+  openMobileMenu(): void {
+    this.mobileMenuOpen.set(true);
+  }
+
+  closeMobileMenu(): void {
+    this.mobileMenuOpen.set(false);
+  }
+}
