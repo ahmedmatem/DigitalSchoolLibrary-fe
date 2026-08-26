d65bbd1fedc11f5606738315a05331bb4b467524
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-26 19:49:01 +0100

    Create public-resource-dto

---
+++ src/app/features/resource-details/models/public-resource-details.dto.ts
@@ -0,0 +1,20 @@
+import { ResourceType, } from '../../../core/models/resource-type.model';
+
+export interface PublicResourceDetailsDto {
+  id: string;
+  title: string;
+  description: string;
+  author: string | null;
+
+  type: ResourceType;
+
+  subjectName: string;
+  categoryName: string;
+
+  audienceType: number;
+
+  hasCover: boolean;
+  requiresAuthentication: boolean;
+
+  createdAtUtc: string;
+}
